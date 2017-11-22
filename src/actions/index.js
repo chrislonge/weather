34c45cbe50c353c0188e52521f6bbc60b6d5b450
Chris Longe <chris.longe@capitalone.com>
2017-11-22 18:12:45 -0500

    Remove console.log

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,10 +8,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
-    console.log('Request: ', request);
     
     return {
         type: FETCH_WEATHER,
-        payload: request
+        payload: request    // request is a promise
     };
 }
