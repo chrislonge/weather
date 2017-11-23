c1c3d19705a3c6f0429ba23f92eeedb8bfea9b67
Chris Longe <chris.longe@capitalone.com>
2017-11-22 19:12:15 -0500

    Upgrades Sparklines package and displays single chart.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -33,7 +33,7 @@ class SearchBar extends Component {
         return (
             <form onSubmit={this.onFormSubmit} className="input-group">
                 <input 
-                    placeholder="Get a five-day forecase in your favorite cities"
+                    placeholder="Get a five-day forecast in your favorite cities"
                     className="form-control"
                     value={this.state.term}
                     onChange={this.onInputChange} />
